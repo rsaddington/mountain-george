69b21e525c912dba63b318784d006704d274d811
Rich Saddington <rich.saddington@Richs-MacBook-Pro.local>
2019-01-29 16:18:56 +0000

    BLT-123: Another refactor from Ben.

--- docroot/modules/custom/acquia_analytics_redirects/src/EventSubscriber/AnalyticsRedirectsEventSubscriber.php
+++ docroot/modules/custom/acquia_analytics_redirects/src/EventSubscriber/AnalyticsRedirectsEventSubscriber.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\Component\Utility\UrlHelper;
 
 /**
  * Subscriber for appending Stripped Query String back on to redirect URL.
@@ -16,16 +17,18 @@ class AnalyticsRedirectsEventSubscriber implements EventSubscriberInterface {
    * {@inheritdoc}
    */
   public function getHeaderAcquiaStrippedQuery(FilterResponseEvent $event) {
+
     /* @var $response \Symfony\Component\HttpFoundation\RedirectResponse */
     $response = $event->getResponse();
 
     if ($response->getStatusCode() == 301 || $response->getStatusCode() == 302) {
       $request = $event->getRequest();
       $headers = $request->headers;
+
       if (!empty($query_string = $headers->get('X-Acquia-Stripped-Query'))) {
         $target = $response->getTargetUrl();
-        $glue = (strpos($target, '?') === FALSE ? '?' : '&');
-        $target = $target . $glue . $query_string;
+        $url_parts = UrlHelper::parse($target);
+        $target = $url_parts['path'] . '?' . $query_string;
         $event->setResponse(new RedirectResponse($target, $response->getStatusCode()));
       }
     }
