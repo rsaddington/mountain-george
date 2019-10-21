794e407aa3c03ae25717a072baa7950212b19441
Rich Saddington <rich.saddington@Richs-MacBook-Pro.local>
2019-10-21 09:38:22 +0100

    BLT-123: Reverting settings to sensible values.

--- docroot/sites/settings/rich.settings.php
+++ docroot/sites/settings/rich.settings.php
@@ -1,7 +1,9 @@
 <?php
 
+// This is an example settings file to investigate how settings can be overridden for different environments.
+
 if($_ENV['AH_SITE_ENVIRONMENT'] == "dev") {
-	$config['system.site']['name'] = 'Rich Settings and Dev';
+	//$config['system.site']['name'] = 'Rich Settings and Dev';
 }
 
 //$config['system.site']['name'] = 'Rich Settings';
